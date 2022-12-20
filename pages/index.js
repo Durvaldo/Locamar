aad44af560703c62204fda724b6c2b99364ee7bf
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2022-12-20 15:57:45 -0300

    cabeçalho

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 function HomePage() {
     return (
-        <section className="bg-locamar-black">  
-            <img
-                src="https://via.placeholder.com/200x100"
-                alt="Picture of the author"
-            /> 
-            <h1 className=""></h1>
+        <section className="bg-locamar-primary shadow-sm shadow-gray-500">  
+            <div className="py-3 flex justify-around items-center">
+                <img
+                    src="/images/locamar-logo.png"
+                    alt="Logo Locamar"
+                    className="max-h-40"
+                />
+                <ul className="hidden md:flex flex-col sm:flex-row gap-2">
+                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Inicio</li>
+                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Sobre nós</li>
+                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Nossos Serviços</li>
+                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Onde Estamos</li>
+                </ul>
+                <div class="p-4 space-y-2 bg-gray-600 rounded shadow md:hidden cursor-pointer">
+                    <span class="block w-8 h-0.5 bg-locamar-white"></span>
+                    <span class="block w-8 h-0.5 bg-locamar-white"></span>
+                    <span class="block w-8 h-0.5 bg-locamar-white"></span>
+                </div>
+            </div>
         </section>
     )
 }
   
-  export default HomePage
+export default HomePage
