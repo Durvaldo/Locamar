e8110707e92f748c68c3270065da876524fe3dbc
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2022-12-21 16:57:01 -0300

    Incorporando o fontawsome e começando o drop menu

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
 function HomePage() {
+    const abrirDropDown = () => {
+        const lista = document.getElementById('drop-down-menu')
+        console.log(lista)
+    }
+
     return (
         <section className="bg-locamar-primary shadow-sm shadow-gray-500">  
             <div className="py-3 flex justify-around items-center">
-                <img
-                    src="/images/locamar-logo.png"
-                    alt="Logo Locamar"
-                    className="max-h-40"
-                />
-                <ul className="hidden md:flex flex-col sm:flex-row gap-2">
-                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Inicio</li>
-                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Sobre nós</li>
-                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Nossos Serviços</li>
-                    <li className="bg-locamar-white text-center rounded-full text-bold py-1 px-3 sm:min-w-lg shadow-sm shadow-gray-500 cursor-pointer hover:scale-105">Onde Estamos</li>
-                </ul>
-                <div class="p-4 space-y-2 bg-gray-600 rounded shadow md:hidden cursor-pointer">
-                    <span class="block w-8 h-0.5 bg-locamar-white"></span>
-                    <span class="block w-8 h-0.5 bg-locamar-white"></span>
-                    <span class="block w-8 h-0.5 bg-locamar-white"></span>
-                </div>
+                <img src="/images/locamar-logo.png" alt="Logo Locamar" className="max-h-20 md:max-h-40"/>
+                <nav>
+                    <ul>
+                        <li onClick={abrirDropDown}><FontAwesomeIcon className="md:hidden cursor-pointer scale-x-150" icon={faBars} />
+                            <ul className="hidden md:flex flex-col sm:flex-row gap-2" id='drop-down-menu'>
+                                <li className="locamar-tag">Inicio</li>
+                                <li className="locamar-tag">Sobre nós</li>
+                                <li className="locamar-tag">Nossos Serviços</li>
+                                <li className="locamar-tag">Onde Estamos</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </section>
     )
